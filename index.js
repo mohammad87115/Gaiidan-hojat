0f496a929f58cde3be7fed654e5f878c3e203b65
SextyNine <109962271+mohammad87115@users.noreply.github.com>
2024-03-04 15:25:24 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const { QuickDB } = require("quick.db");
+const { JSONDriver } = require("quick.db");
+const axios = require("axios")
+const jsonDriver = new JSONDriver();
+const db = new QuickDB({ driver: jsonDriver });
+
+(async () => {
+    await db.init();
+})()
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+app.use(express.static('public'));
+
+let count = 5;
+
+app.get("/count", async (req, res) => {
+    
+    const count = await db.get("count")
+    res.json({"data":count})
+})
+
+app.post("/add", async (req, res) => {
+    let capt = req.body['g-recaptcha-response'];
+    console.log(capt)
+    if (!capt) return res.send("INVALID CAPTCHA");
+
+    const cap = await axios.post("https://www.google.com/recaptcha/api/siteverify", `secret=6LfVuk4pAAAAAI24DPD0PNtEUkJMqnyuRi_l_ryh&response=${capt}`)
+
+    console.log(cap.data)
+
+    if (cap.data.success) {
+        await db.add("count", 1)
+
+        await res.redirect("/")
+    } else {
+        await res.send("INVALID CAPTCHA")
+    }
+})
+
+app.listen(5000, () => {
+    console.log('listening on *:80');
+});
